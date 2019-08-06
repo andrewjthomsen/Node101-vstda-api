6383cedbd5dcff44449ab07e5e875b65250bf178
andrew <andrewjthomsen@yahoo.com>
2019-08-05 19:11:46 -0700

    Wrote code for CRUD operations. Having an issue passing one test. Plan to resolve before finished.

---
+++ server/app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const morgan = require('morgan');
+const router = new express.Router();
+
+
+const app = express();
+app.use(morgan("dev"));
+
+// mock data
+var data = [
+    {todoItemId: 0,
+        name: "an item",
+        priority: 3,
+        completed: false
+    },
+    {
+        todoItemId: 1,
+        name: "another item",
+        priority: 2,
+        completed: false
+    },
+    {
+        todoItemId: 2,
+        name: "a done item",
+        priority: 1,
+        completed: true
+    }
+];
+// GET request being sent to server will return with data
+
+
+// add your code here
+app.get("/", (req, res) => {
+    res.status(200)
+    res.send({ status: "ok"})
+});
+
+// READ ALL TODO ITEMS FROM LIST
+app.get("/api/TodoItems", (req, res) => {
+    res.status(200)
+    res.json(data);
+});
+
+// READ SINGLE TODO ITEM FROM LIST
+app.get("/api/TodoItems/:number", (req, res) => {
+    res.json(data.find(function (item) {
+        return item.todoItemId == req.params.number
+    }));
+});
+// CREATE SINGLE TODO ITEM
+// app.post() responsible for creating item
+app.post("/api/TodoItems/", (req, res) => {
+    let postedID = req.body.todoItemId;
+    let foundItem = false;
+
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].todoItemId === postedID) {
+            data[i] = req.body;
+            foundItem = true;
+        }
+    }
+    if (foundItem === fale) {
+        data.push(req.body)
+
+    }
+    res.status(201).json(req.body);
+});
+
+// DELETE TODO ITEM
+app.delete("/api/TodoItems/:number", (req, res) => {
+    let deleteItem = req.params.number;
+    for (var i =0; i < data.length; i++) {
+        if (data[i].todoItemId == deleteItem) {
+            res.json(data[i]);
+            data.splice(i, 1);
+        }
+    }
+});
+
+module.exports = app;
